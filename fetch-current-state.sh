9e734c8a822fc4ae189916b1f96625d300c2eed9
Matt Novinger <mnovinger@gmail.com>
2021-12-20 16:14:02 -0700

    fixing AC scripts, crontab

--- fetch-current-state.sh
+++ fetch-current-state.sh
@@ -1,4 +1,7 @@
 # ---> sudo vi /etc/ssmtp/ssmtp.conf
 #wget -S -O - --header="Accept-Encoding: gzip, deflate" http://10.0.0.1/state >> "Subject: Yeti Status\n\n" | ssmtp matt@novinger.one
 #printf "Subject: Yeti Status\n\n %s", `wget -S -O - --header="Accept-Encoding: gzip, deflate" http://10.0.0.1/state | tr -d '\n'`
-printf "Subject: Yeti Status\n\n %s", `cat test.txt|tr --delete '\n'`
+#printf "Subject: Yeti Status\n\n %s", `cat test.txt|tr --delete '\n'`
+
+
+wget -S -O - --header="Accept-Encoding: gzip, deflate" http://10.1.1.1/state 
